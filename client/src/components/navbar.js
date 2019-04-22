8ac7d90894bfd79d19c7e78dc516a4b5d0ec452b
David Dropping <ddroppin@mail.sfsu.edu>
2019-04-22 12:21:17 -0700

    create navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav class='navbar navbar-expand-sm navbar-light bg-light'>
+          <div className='container'>
+            <a class='navbar-brand' href='#'>
+              <strong>NAVBAR</strong>
+            </a>
+            <button
+              class='navbar-toggler'
+              type='button'
+              data-toggle='collapse'
+              data-target='#navbarNav'
+              aria-controls='navbarNav'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+            >
+              <span class='navbar-toggler-icon' />
+            </button>
+            <div class='collapse navbar-collapse' id='navbarNav'>
+              <ul class='navbar-nav'>
+                <li class='nav-item'>
+                  <a class='nav-link' href='#'>
+                    Todo
+                  </a>
+                </li>
+                <li class='nav-item'>
+                  <a class='nav-link' href='#'>
+                    Features
+                  </a>
+                </li>
+                <li class='nav-item'>
+                  <a class='nav-link' href='#'>
+                    Pricing
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
